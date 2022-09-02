82a82f85944746f68cb1585deba2c4806ffe4624
sam <sampurna.chapagainnn@gmail.com>
2022-09-02 10:43:28 +0100

    overall changes

--- Anigram/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('', view=views.getRoutes, name="routes"),
-    path('posts/', views.getPosts, name='posts'),
-    path('posts/<str:pk>', views.getPost, name='post')
-
-]
