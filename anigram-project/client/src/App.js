02a6ad118b251d1c4c3d2a2cdee9da3d757a340b
codingkompot <ksqasiyah@protonmail.com>
2022-09-05 12:32:06 +0100

    merge

--- anigram-project/client/src/App.js
+++ anigram-project/client/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 // import { PageWrapper } from "./layout";
-import * as Pages from "./pages";
+import * as Pages from "./Pages";
 
 const App = () => {
-    return (
-        <div className="App">
-             <Routes>
-             {/* <Route exact path="/" element={<PageWrapper />}> */}
-                <Route exact path="/" element={<Pages.Home />} />
-                <Route exact path="/register" element={<Pages.Register />} />
-                <Route exact path="/create" element={<Pages.CreatePost />} />
-                <Route exact path="/login" element={<Pages.Login />} />
-                <Route exact path="/dashboard" element={<Pages.Dashboard />} />
-                <Route exact path ="/main" element={<Pages.MainPage />}/>
-                
-                <Route exact path="*" element={<Pages.NotFoundPage />} />
-                {/* </Route> */}
-            </Routes> 
-        </div>
-    )
-  }
+  return (
+    <div className="App">
+      <Routes>
+        {/* <Route exact path="/" element={<PageWrapper />}> */}
+        <Route exact path="/" element={<Pages.Home />} />
+        <Route exact path="/register" element={<Pages.Register />} />
+        <Route exact path="/create" element={<Pages.CreatePost />} />
+        <Route exact path="/login" element={<Pages.Login />} />
+        <Route exact path="/dashboard" element={<Pages.Dashboard />} />
+        <Route exact path="/main" element={<Pages.MainPage />} />
+
+        <Route exact path="*" element={<Pages.NotFoundPage />} />
+        {/* </Route> */}
+      </Routes>
+    </div>
+  );
+};
 export default App;
