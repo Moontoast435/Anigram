6e6b735b17aa619b922ac0cc71cb0a93f86c28b1
Moontoast435 <seanmalone1993@gmail.com>
2022-09-08 20:28:00 +0100

    settings.py

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'anigram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,12 +137,13 @@ STATIC_URL = '/static/'
 
 
 # STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
