1b493cca9a44aa030180cd0b789154f40364b789
Moontoast435 <seanmalone1993@gmail.com>
2022-09-08 19:48:35 +0100

    changed settings.py

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -96,12 +96,7 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
