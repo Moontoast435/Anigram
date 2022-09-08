38e23465cb8e39a786ae63e1347ee4a49f02aec5
Matthew Richardson <matt.rich2045@gmail.com>
2022-09-08 20:46:04 +0100

    changed stuff based on the article

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -15,6 +15,7 @@ import os
 import django_heroku
 import dj_database_url
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -164,5 +165,5 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
