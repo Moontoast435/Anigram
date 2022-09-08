1eef990de98768437bc9fde19b512d342fdf02a6
Moontoast435 <seanmalone1993@gmail.com>
2022-09-08 21:46:33 +0100

    settings.py

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 
 
 # STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
