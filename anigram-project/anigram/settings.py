2fe52d71f7c011f156cb25767d5d64d70283ff5e
Matthew Richardson <matt.rich2045@gmail.com>
2022-09-08 20:50:24 +0100

    reverted views.py change

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -15,7 +15,6 @@ import os
 import django_heroku
 import dj_database_url
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
