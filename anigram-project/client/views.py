5203f1d0ecc386aa1c415c8ad1731ffb4e45155c
Moontoast435 <seanmalone1993@gmail.com>
2022-09-08 20:18:39 +0100

    changed views.py in client

--- anigram-project/client/views.py
+++ anigram-project/client/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
