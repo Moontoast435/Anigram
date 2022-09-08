b3e13bb696742c03d964275f714dadf99d6231eb
Matthew Richardson <matt.rich2045@gmail.com>
2022-09-08 21:06:40 +0100

    added a different url pattern

--- anigram-project/anigram/urls.py
+++ anigram-project/anigram/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [re_path(r'^.*(?!(media))', TemplateView.as_view(template_name='index.html'))]
+# urlpatterns += [re_path(r'^.*(?!(media))', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path('.*', TemplateView.as_view(template_name='index.html'))]
