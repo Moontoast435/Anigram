ecf65b0bc15893ce5305ef5a6f026afe96edc14c
Matthew Richardson <matt.rich2045@gmail.com>
2022-09-08 21:00:10 +0100

    added HTTPresponse for index

--- anigram-project/client/views.py
+++ anigram-project/client/views.py
@@ -5,9 +5,16 @@ from django.http import HttpResponse, HttpResponseNotFound
 import os
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
+def index(request):
+    path = os.path.join(os.path.dirname(__file__), 'static', 'client/main.js')
+    if os.path.isfile(path):
+        with open(path, 'rb') as file:
+            return HttpResponse(file.read(), content_type='application/javascript')
+    else:
+        return HttpResponseNotFound()
 
 # from django.shortcuts import render
 # from rest_framework import viewsets
@@ -17,8 +24,8 @@ def index(request):
 # from django.http import HttpResponse, HttpResponseNotFound
 # import os
 
-# # The viewsets base class provides the implementation for CRUD operations by default,
-# # what we had to do was specify the serializer class and the query set.
+# The viewsets base class provides the implementation for CRUD operations by default,
+# what we had to do was specify the serializer class and the query set.
 # class TodoView(viewsets.ModelViewSet):
 #     serializer_class = TodoSerializer
 #     queryset = Todo.objects.all()
