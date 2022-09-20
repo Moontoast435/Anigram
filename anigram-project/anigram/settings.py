e9b450e29219eff42af405fe273ff7253a1baf12
Moontoast435 <seanmalone1993@gmail.com>
2022-09-20 12:30:50 +0100

    change settings.py

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -101,7 +101,7 @@ CHANNEL_LAYERS = {
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(        # Feel free to alter this value to suit your needs.        
-default='postgresql://postgres:postgres@localhost:5432/mysite',        
+default='postgresql://postgres:postgres@localhost:5432/anigram',        
 conn_max_age=600    
 )
 
