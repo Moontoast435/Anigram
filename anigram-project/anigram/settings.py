93d3b57afbb3f2231b2e7b542745ca1fb0340103
Moontoast435 <seanmalone1993@gmail.com>
2022-09-20 18:33:18 +0100

    settings.py

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
@@ -146,7 +146,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 if not DEBUG: 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
