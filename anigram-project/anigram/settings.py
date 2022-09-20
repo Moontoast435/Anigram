3f916850ac4b5bfc35afc0ff35e1a5ddff5bf3cc
Moontoast435 <seanmalone1993@gmail.com>
2022-09-20 18:16:37 +0100

    settings.py change

--- anigram-project/anigram/settings.py
+++ anigram-project/anigram/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 if not DEBUG: 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
